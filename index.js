7b0c9371d16c15129ada9cb9a461606af7174f6e
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-16 15:16:11 +0500

    Implement HTTP server in index.js and update package.json to use ES modules

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("neeraj");
+import http from "http";
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello this our first server");
+})
+
+server.listen(3000, ()=>{
+    console.log("server is running on port 3000");
+});
+
