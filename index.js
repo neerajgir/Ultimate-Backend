7a13d92bbc40bd7765029efe0dffd2f5d4a24d23
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-16 15:28:19 +0500

    Refactor server response handling in index.js to serve different pages

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import http from "http";
 
 const server = http.createServer((req, res)=>{
-    res.end("Hello this our first server");
+    if(req.url=="/"){
+        res.end("Welcome to Homepage")
+    }else if(req.url == "/about"){
+        res.end("Welcome to About Page")
+    }else if(req.url == "/contact"){
+        res.end("Welcome to Contact Page")
+    }else{
+        res.end("404 Not Found")
+    }
 })
 
 server.listen(3000, ()=>{
