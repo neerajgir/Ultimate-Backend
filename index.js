fc48c0ce314641eaf9e78ddcf74b3a212772cbdb
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-17 15:25:39 +0500

    Fix response message casing in index.js and update package.json to use nodemon for development

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res)=>{
-    res.send("Hello Welcome To express")
+    res.send("Hello Welcome To Express")
+})
+app.post("/about", (req, res)=>{
+    const body = req.body
+    console.log(body);
+    res.send("Hello Welcome To About Page")
+})
+app.get("/contact", (req, res)=>{
+    res.send("Hello Welcome To Contact Page")
 })
 
 app.listen(3000, ()=>{
