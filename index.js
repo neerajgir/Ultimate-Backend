7d770e236faf1a5ddd2facf919447a60199a9599
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-20 15:28:01 +0500

    Add user model and create user endpoint in index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
+import User from "./models/user.model.js";
 
 
 const app = express();
 app.use(express.json());
 
-const MONGODB_URI = "mongodb+srv://neeraj:Neeraj@cluster0.vk19rqu.mongodb.net/UltimateBackend"
+const MONGODB_URI = "mongodb+srv://neeraj:neeraj@cluster0.vk19rqu.mongodb.net/UltimateBackend"
 
 const connectDB = async () => {
   try {
@@ -16,45 +17,59 @@ const connectDB = async () => {
   }
 }
 
-app.get("/", (req, res)=>{
-    res.send("Hello Welcome To Express")
-})
-app.post("/about", (req, res)=>{
-    const body = req.body
-    console.log(body);
-    res.send("Hello Welcome To About Page")
-})
-app.get("/contact", (req, res)=>{
-    res.send("Hello Welcome To Contact Page")
-})
+// app.get("/", (req, res)=>{
+//     res.send("Hello Welcome To Express")
+// })
+// app.post("/about", (req, res)=>{
+//     const body = req.body
+//     console.log(body);
+//     res.send("Hello Welcome To About Page")
+// })
+// app.get("/contact", (req, res)=>{
+//     res.send("Hello Welcome To Contact Page")
+// })
 
 
-app.get("/user/:id", (req,res)=>{
-    const id = req.params.id
-    const existingUser = users.find((user)=>user.id == id)
-    res.json(existingUser)
-})
+// app.get("/user/:id", (req,res)=>{
+//     const id = req.params.id
+//     const existingUser = users.find((user)=>user.id == id)
+//     res.json(existingUser)
+// })
 
-app.get("/search",(req,res)=>{
-    const query =  req.query
-    res.json(query)
-})
+// app.get("/search",(req,res)=>{
+//     const query =  req.query
+//     res.json(query)
+// })
 
-const password = "Neeraj122";
+// const password = "Neeraj122";
 
-app.use((req,res,next)=>{
-  if(req.body.pass !== password){
-    res.send("pass does not match")
+// app.use((req,res,next)=>{
+//   if(req.body.pass !== password){
+//     res.send("pass does not match")
+//   }
+//   next()
+// })
+
+// app.post("/",(req,res)=>{
+//   console.log(req.body);
+//   console.log(req.headers);
+//   res.status(200).send({success: true})
+// })
+
+app.post("/create", async (req,res)=>{
+  try {
+    let {name, age, email, userName} = req.body
+    await User.create({
+      name,
+      age,
+      email,
+      userName
+    })
+    res.status(201).json({message: "User Created"})
+  } catch (error) {
+    res.status(400).json({message: error})
   }
-  next()
 })
-
-app.post("/",(req,res)=>{
-  console.log(req.body);
-  console.log(req.headers);
-  res.status(200).send({success: true})
-})
-
 
 app.listen(3000, ()=>{
     connectDB()
---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    userName: {
+        type: String,
+        require: true,
+        unique: true
+    }
+},{timestamps: true})
+
+const User = mongoose.model("User", userSchema);
+
+export default User
